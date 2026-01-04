cf1e590b01c86c2c861e93b3465bc0178fb762f0
Adimas Putra Istiawan <adimas@suitmedia.com>
2026-01-04 14:11:26 +0800

    adjust: profile image store

--- src/profile/multer.config.ts
+++ src/profile/multer.config.ts
@@ -2,9 +2,15 @@ import { existsSync, unlinkSync } from 'fs';
 import { diskStorage } from 'multer';
 import { extname, join } from 'path';
 import { v4 as uuid } from 'uuid';
+import * as fs from 'fs';
+import * as path from 'path';
 
 export const profileImageStorage = diskStorage({
-  destination: './uploads/profiles',
+  destination: (req, file, cb) => {
+    const uploadPath = path.join(process.cwd(), 'uploads/profiles');
+    ensureDir(uploadPath);
+    cb(null, uploadPath);
+  },
   filename: (_, file, cb) => {
     const uniqueName = `${uuid()}${extname(file.originalname)}`;
     cb(null, uniqueName);
@@ -18,5 +24,11 @@ export function deleteFile(filePath?: string) {
 
   if (existsSync(fullPath)) {
     unlinkSync(fullPath);
+  }
+}
+
+export function ensureDir(dirPath: string) {
+  if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath, { recursive: true });
   }
 }
